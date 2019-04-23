2b4aa214592f0889cf9f7bd59ed4b9f01ebf5972
nhack <cristea12@gmail.com>
2019-04-23 17:42:18 +0300

    01 - using from to create an observable

--- index.ts
+++ index.ts
@@ -1,17 +1,9 @@
-import { Observable, Subscriber } from 'rxjs';
+import { Observable, Subscriber, from } from 'rxjs';
 
 import { IPizza } from './pizza/pizza.model';
 import { PIZZAS } from './pizza/pizza.data';
 
-// this method is called whit the first subscription
-const subscribe = (subscriber: Subscriber<IPizza>) => {
-  for (let pizza of PIZZAS) {
-    subscriber.next(pizza);
-  }
-  subscriber.complete();
-}
-
-const pizzas$ = new Observable(subscribe);
+const pizzas$ = from(PIZZAS);
 
 // register the subscriber on the observable
 pizzas$.subscribe({
