8769744be33d6a968609197a4e836c1a6086589c
nhack <cristea12@gmail.com>
2020-05-18 22:50:47 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import { IPizza } from './pizza/pizza.model';
 import { PIZZAS } from './pizza/pizza.data';
 
 // this method is called whit the first subscription
-const subscribe = (subscriber: Subscriber<IPizza>) => {
+const generator = (subscriber: Subscriber<IPizza>) => {
   for (let pizza of PIZZAS) {
     subscriber.next(pizza);
   }
 }
 
-const pizzas$ = new Observable(subscribe);
+const pizzas$ = new Observable(generator);
 
 // register the subscriber on the observable
 pizzas$.subscribe(pizza => console.log(pizza.name));
