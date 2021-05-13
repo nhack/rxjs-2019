b5a6ed65ba480edeea61bc1dd3cfe501aebf1201
nhack <cristea12@gmail.com>
2021-05-13 11:54:54 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import 'rxjs/add/operator/map';
 
 const currentTime$: Observable<Date> = new Observable(subscriber => {
   setInterval(() => subscriber.next(new Date()), 1000);
 });
 
-const toTime = (date: Date) => date.getTime()
+const toTime = (date: Date) => date.getTime();
 const toTimeMap = map(toTime);
 
 toTimeMap(currentTime$).subscribe(time => console.log('Manual:' + time));
-currentTime$.map(toTime).subscribe(time => console.log('Chain:' + time));
 currentTime$.pipe(toTimeMap).subscribe(time => console.log('Pipe:' + time));
