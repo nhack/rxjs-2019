52be317682fd87b85f21fc3b71c95c8dcc1ead2a
nhack <cristea12@gmail.com>
2021-05-13 15:49:36 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -6,8 +6,7 @@ const source$ = interval(1000).pipe(take(4));
 source$.subscribe(value => console.log(`Observer 1: ${value}`));
 setTimeout(() => source$.subscribe(value => console.log(`Observer 2: ${value}`)), 1000);
 setTimeout(() => source$.subscribe(value => console.log(`Observer 3: ${value}`)), 2000);
-setTimeout(() => source$.subscribe(
-  value => console.log(`Observer 4: ${value}`),
-  null,
-  () => console.log('Observer 4 complete.')
-), 4500);
+setTimeout(() => source$.subscribe({
+  next: value => console.log(`Observer 4: ${value}`),
+  complete: () => console.log('Observer 4 complete.')
+  }), 4500);
